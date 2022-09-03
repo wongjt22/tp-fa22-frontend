603a7e85ccf8ddefc9f137202ab04f1b3ab8b96b
Emir  Karabeg <emirkarabeg@Emirs-MacBook-Pro.local>
2022-09-03 11:58:56 -0700

    Repo setup

---
+++ src/pages/landing.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Landing: NextPage = () => {
+  return <></>;
+};
+
+export default Landing;
---
+++ src/pages/victors.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Victors: NextPage = () => {
+  return <></>;
+};
+
+export default Victors;
